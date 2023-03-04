f2e8dee83dea2afa991f2e75d19e5c555d474ed5
burtakov <burtakov@wireless.iitp.ru>
2023-03-04 11:50:33 +0300

    Added add and _row2obj func

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -24,3 +24,27 @@ class SQLiteRepository(AbstractRepository[T]):
         self.fields = get_annotations(cls, eval_str=True)
         self.fields.pop('pk')
         self.obj_cls = cls
+
+    def add(self, obj: T) -> int:
+        if getattr(obj, 'pk', None) != 0:
+            raise ValueError(f'trying to add object {obj} with filled `pk` attribute')
+        names = ', '.join(self.fields.keys())
+        questions = ', '.join("?" * len(self.fields))
+        values = [getattr(obj, f) for f in self.fields]
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute('PRAGMA foreign_keys = ON')
+            cur.execute(
+                f'INSERT INTO {self.table_name} ({names}) VALUES({questions})',
+                values
+            )
+            obj.pk = cur.lastrowid
+        con.close()
+        return obj.pk
+    
+    def _row2obj(self, rowid: int, row: tuple[Any]) -> T:
+        """ Конвертирует строку из БД в объект типа Т """
+        kwargs = dict(zip(self.fields, row))
+        obj = self.obj_cls(**kwargs)
+        obj.pk = rowid
+        return obj
