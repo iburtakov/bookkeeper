4ba65e8582c49d018a9bf44dea6db5d77860e4fc
burtakov <burtakov@wireless.iitp.ru>
2023-03-05 21:13:12 +0300

    added delete, update, get_all

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -41,10 +41,70 @@ class SQLiteRepository(AbstractRepository[T]):
             obj.pk = cur.lastrowid
         con.close()
         return obj.pk
-    
+
     def _row2obj(self, rowid: int, row: tuple[Any]) -> T:
         """ Конвертирует строку из БД в объект типа Т """
         kwargs = dict(zip(self.fields, row))
         obj = self.obj_cls(**kwargs)
         obj.pk = rowid
         return obj
+
+    def get(self, pk: int) -> T | None:
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            row = cur.execute(
+                f'SELECT * FROM {self.table_name} '
+                + f'WHERE ROWID=={pk}'
+            ).fetchone()
+        con.close()
+        if row is None:
+            return None
+        obj = self._row2obj(pk, row)
+        return obj
+
+    def get_all(self, where: dict[str, Any] | None = None) -> list[T]:
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            if where is None:
+                rows = cur.execute(
+                    f'SELECT ROWID, * FROM {self.table_name} '
+                ).fetchall()
+            else:
+                fields = " AND ".join([f"{f} LIKE ?" for f in where.keys()])
+                rows = cur.execute(
+                    f'SELECT ROWID, * FROM {self.table_name} '
+                    + f'WHERE {fields}',
+                    list(where.values())
+                ).fetchall()
+        con.close()
+        return [self._row2obj(r[0], r[1:]) for r in rows]
+
+    def get_all_like(self, like: dict[str, str]) -> list[T]:
+        values = [f"%{v}%" for v in like.values()]
+        where = dict(zip(like.keys(), values))
+        return self.get_all(where=where)
+
+    def update(self, obj: T) -> None:
+        fields = ", ".join([f"{f}=?" for f in self.fields.keys()])
+        values = [getattr(obj, f) for f in self.fields]
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute(
+                f'UPDATE {self.table_name} SET {fields} '
+                + f'WHERE ROWID=={obj.pk}',
+                values
+            )
+            if cur.rowcount == 0:
+                raise ValueError('attempt to update object with unknown primary key')
+        con.close()
+
+    def delete(self, pk: int) -> None:
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute(
+                f'DELETE FROM  {self.table_name} '
+                + f'WHERE ROWID=={pk}'
+            )
+            if cur.rowcount == 0:
+                raise ValueError('attempt to delete object with unknown primary key')
+        con.close()
