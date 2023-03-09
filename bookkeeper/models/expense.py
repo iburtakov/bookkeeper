dd7253682c2f73c712bcfa34204208384d5b7d88
burtakov <burtakov@wireless.iitp.ru>
2023-03-09 11:45:36 +0300

    added timespec for expense

--- bookkeeper/models/expense.py
+++ bookkeeper/models/expense.py
@@ -19,7 +19,7 @@ class Expense:
     """
     amount: int
     category: int
-    expense_date: str = datetime.now().isoformat()
-    added_date: str = datetime.now().isoformat()
+    expense_date: str = datetime.now().isoformat(sep='\t', timespec='minutes')
+    added_date: str = datetime.now().isoformat(sep='\t', timespec='minutes')
     comment: str = ''
     pk: int = 0
